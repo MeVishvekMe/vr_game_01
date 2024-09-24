828d7a5731c3bc94882e0be2e048202de626c675
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-09-24 12:38:39 +0530

    -

--- Assets/Scripts/SmoothClimb.cs
+++ Assets/Scripts/SmoothClimb.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -12,8 +13,8 @@ public class SmoothClimb : MonoBehaviour
     private Rigidbody Playerrigidbody;
     private Vector3 PlayerVelocity;
     [SerializeField] private float PushFactor = 1.5f;
-    //[SerializeField] private float distance = 5.0f;
-    //[SerializeField] private float speed = 4.0f;
+    [SerializeField] private float distance = 5.0f;
+    [SerializeField] private float speed = 4.0f;
     private void Awake()
     {   
         OnValidate();
@@ -36,17 +37,23 @@ public class SmoothClimb : MonoBehaviour
             TryGetComponent(out interactor);
         
     }
+
+    private void Update()
+    {
+        Debug.Log("player velocity"+ Playerrigidbody.velocity);
+    }
     private void PushUpward(SelectExitEventArgs arg0)
     {
         if (!arg0.interactableObject.transform.TryGetComponent(out ClimbInteractable climbInteractable)) return;
             if (Playerrigidbody)
             {
                 PlayerVelocity = Playerrigidbody.velocity;
-                if (PlayerVelocity.y > 0)
+                if (PlayerVelocity.y < 0)
                 {
                     Vector3 force = new Vector3(0, PlayerVelocity.y * PushFactor, 0);
-                    Playerrigidbody.AddForce(force, ForceMode.Impulse);
-                    //PhysicRig.transform.position = Vector3.MoveTowards(PhysicRig.transform.position, new Vector3(0, distance, 0), speed);
+                   // Playerrigidbody.AddForce(force, ForceMode.Force);
+                    PhysicRig.transform.position = Vector3.MoveTowards(PhysicRig.transform.position, PhysicRig.transform.position + new Vector3(0,distance,0), speed*Time.deltaTime);
+                    Debug.Log(" force added ");
                 }
             }
     }
@@ -55,7 +62,7 @@ public class SmoothClimb : MonoBehaviour
         if (!arg0.interactableObject.transform.TryGetComponent(out ClimbInteractable climbInteractable)) return;
         if (Playerrigidbody)
         {
-            Playerrigidbody.velocity = Vector3.zero;
+            //Playerrigidbody.velocity = Vector3.zero;
         }
     }
     private void OnDestroy()
