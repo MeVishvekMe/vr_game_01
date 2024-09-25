d2c85a30035c8dc58cc01472d1389f0c93a449d2
Vishvek Methwani <methwani.vishvek09@gmail.com>
2024-09-25 13:18:52 +0530

    progress2

--- Assets/Scripts/PhysicRig.cs
+++ Assets/Scripts/PhysicRig.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PhysicRig : MonoBehaviour {
     public Transform playerHead;
+    public Transform leftController;
+    public Transform rightController;
+
+    public ConfigurableJoint headJoint;
+    public ConfigurableJoint leftHandJoint;
+    public ConfigurableJoint rightHandJoint;
     public CapsuleCollider bodyCollider;
 
     public float bodyMinHeight = 0.5f;
@@ -11,5 +17,11 @@ public class PhysicRig : MonoBehaviour {
     void FixedUpdate() {
         bodyCollider.height = Mathf.Clamp(playerHead.localPosition.y, bodyMinHeight, bodyMaxHeight);
         bodyCollider.center = new Vector3(playerHead.localPosition.x, bodyCollider.height / 2, playerHead.localPosition.z);
+        leftHandJoint.targetPosition = leftController.localPosition;
+        leftHandJoint.targetRotation = leftController.localRotation;
+        rightHandJoint.targetPosition = rightController.localPosition;
+        rightHandJoint.targetRotation = rightController.localRotation;
+
+        headJoint.targetPosition = playerHead.localPosition;
     }
 } 
