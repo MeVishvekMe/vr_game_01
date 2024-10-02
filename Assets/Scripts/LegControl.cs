1dee5d117f0277b569b5487d37ed95b043fc32e9
vaibhavrohilla-03 <vaibhav.229301110@muj.manipal.edu>
2024-10-03 00:49:33 +0530

    final changes(ragdoll not setup)

--- Assets/Scripts/LegControl.cs
+++ Assets/Scripts/LegControl.cs
@@ -4,63 +4,64 @@ using UnityEngine;
 
 public class LegControl : MonoBehaviour
 {
-    public Transform leftTarget; 
-    public Transform rightTarget;
-    public Transform Lray;  
-    public Transform Rray; 
-    public Transform Hips; 
+    
+    public Transform leftTarget; //the Ik target for the feft leg
+    public Transform rightTarget; //the Ik target for the right leg
+    public Transform Lray; //the position that a raycast will be sent from to determin where the left foot should go
+    public Transform Rray; //the position that a raycast will be sent from to determin where the left foot should go
+    public Transform Hips; //the hips or chest
  
-    public float LegStepUp; 
+    public float LegStepUp; //the level that your leg should go up before you step
  
-    private Vector3 ShouldBeL;
-    private Vector3 ShouldBeR;
+    private Vector3 ShouldBeL; //where your ik target will always be trying to lerp to
+    private Vector3 ShouldBeR; //where your ik target will always be trying to lerp to
  
-    private Vector3 ShouldReallyBeL; 
-    private Vector3 ShouldReallyBeR;
+    private Vector3 ShouldReallyBeL; //always where the raycast hits or where the foot would end up if it stepped
+    private Vector3 ShouldReallyBeR;//always where the raycast hits or where the foot would end up if it stepped
  
-    public float DistL; 
-    public float DistR;
+    public float DistL; //distance from left foot to ShouldReallyBeL
+    public float DistR; //distance from right foot to ShouldReallyBeR
  
-    private bool LStepping; 
-    private bool RStepping; 
+    private bool LStepping; //true when the left leg is stepping
+    private bool RStepping; //true when the right leg is stepping
  
-    private string LastStep; 
+    private string LastStep; //the last foot that stepped
  
-    public float wantStepAt; 
+    public float wantStepAt; //the distance that you will step at
  
-    public float legSpeed; 
+    public float legSpeed; //lerping speed
  
-   
+    // Start is called before the first frame update
     void Start()
     {
-        ShouldBeL = leftTarget.position; 
-        ShouldBeR = rightTarget.position;
-        LastStep = "R"; 
+        ShouldBeL = leftTarget.position; //just making it not look wierd at the start
+        ShouldBeR = rightTarget.position; //just making it not look wierd at the start
+        LastStep = "R"; //to make your left foot step first because left is good
     }
  
-   
+    // Update is called once per frame
     void Update()
     {
         RaycastHit hitL;
-        if (Physics.Raycast(Lray.position, Vector3.down, out hitL)) 
+        if (Physics.Raycast(Lray.position, Vector3.down, out hitL)) //send a raycast for left foot placement
         {
             ShouldReallyBeL = hitL.point;
         }
         RaycastHit hitR;
-        if (Physics.Raycast(Rray.position, Vector3.down, out hitR)) 
+        if (Physics.Raycast(Rray.position, Vector3.down, out hitR)) //send a raycast for right foot placement
         {
             ShouldReallyBeR = hitR.point;
         }
  
-      
+        // get the distances for the feet
         DistL = Vector3.Distance(ShouldBeL, ShouldReallyBeL);
         DistR = Vector3.Distance(ShouldBeR, ShouldReallyBeR);
  
-       
+        //make stepping smooth
         leftTarget.position = Vector3.Lerp(leftTarget.position, ShouldBeL, legSpeed * Time.deltaTime);
         rightTarget.position = Vector3.Lerp(rightTarget.position, ShouldBeR, legSpeed * Time.deltaTime);
  
-       
+        //figure out wich foot should take a step
         if(!LStepping && !RStepping)
         {
             if(LastStep == "R")
@@ -81,30 +82,31 @@ public class LegControl : MonoBehaviour
             }
         }
  
-   
+        //make the legs y rotation look normal
         leftTarget.transform.eulerAngles = new Vector3(0, Hips.eulerAngles.y, 0);
         rightTarget.transform.eulerAngles = new Vector3(0, Hips.eulerAngles.y, 0);
  
     }
  
-  
+    //left foot stepping function
     IEnumerator stepL()
     {
-        LStepping = true; 
-        ShouldBeL = new Vector3(ShouldBeL.x, ShouldReallyBeL.y + LegStepUp, ShouldBeL.z); 
+        LStepping = true; //tell script that we are stepping
+        ShouldBeL = new Vector3(ShouldBeL.x, ShouldReallyBeL.y + LegStepUp, ShouldBeL.z); //make foot go up
         yield return new WaitForSeconds(0.3f);
-        ShouldBeL = ShouldReallyBeL; 
-        yield return new WaitForSeconds(0.2f); 
-        LStepping = false; 
+        ShouldBeL = ShouldReallyBeL; //place foot in correct position
+        yield return new WaitForSeconds(0.2f); //some extra time so john doesn't step imediatly after
+        LStepping = false; //tell script that we have finished stepping
     }
  
     IEnumerator stepR()
     {
-        RStepping = true;
-        ShouldBeR = new Vector3(ShouldBeR.x, ShouldReallyBeR.y + LegStepUp, ShouldBeR.z); 
+        RStepping = true; //tell script that we are stepping
+        ShouldBeR = new Vector3(ShouldBeR.x, ShouldReallyBeR.y + LegStepUp, ShouldBeR.z); //make foot go up
         yield return new WaitForSeconds(0.3f);
-        ShouldBeR = ShouldReallyBeR; 
-        yield return new WaitForSeconds(0.2f); 
-        RStepping = false; 
+        ShouldBeR = ShouldReallyBeR; //place foot in correct position
+        yield return new WaitForSeconds(0.2f); //some extra time so john doesn't step imediatly after
+        RStepping = false; //tell script that we have finished stepping
     }
+    
 }
